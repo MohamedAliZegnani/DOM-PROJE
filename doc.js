3a759f30dc5e26ec0ab52ce36909e0a115bef009
MohamedAliZegnani <88445785+MohamedAliZegnani@users.noreply.github.com>
2021-08-24 18:22:24 +0200

    Add files via upload

---
+++ doc.js
@@ -0,0 +1,20 @@
+function remove(event){
+    var btn = event.target;
+    btn.parentElement.parentElement.remove();
+}
+var total=0
+document.getElementById("subtotal").innerText= total
+var total=0
+function doc(){
+  total=total+89 
+  document.getElementById("subtotal").innerText= total
+}
+var total1=0
+document.getElementById("subtotale").innerText= total1
+
+function sz(){
+  total1=total1+349
+  document.getElementById("subtotale").innerText= total1
+
+  
+}
